b3df8800b31231b0505621175a45aaefb5ad8f95
Sagnik Banerjee <sagnik2898@gmail.com>
2024-08-08 00:31:42 +0530

    Fix

--- Updated/.next/static/webpack/webpack.517a454e1a1b2910.hot-update.js
+++ Updated/.next/static/webpack/webpack.517a454e1a1b2910.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "64b1b245a0003c08"; }
+/******/ 	__webpack_require__.h = function() { return "2f795a4087213142"; }
 /******/ }();
 /******/ 
 /******/ }
--- Updated/.next/types/app/page.ts
+++ Updated/.next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\sagni\OneDrive\Desktop\All Projects\portfolio\PORTFOLIO-PERS\Updated\src\app\page.js
+// File: C:\Users\sagni\OneDrive\Desktop\All Projects\Portfolio\PORTFOLIO-PERS\Updated\src\app\page.js
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
